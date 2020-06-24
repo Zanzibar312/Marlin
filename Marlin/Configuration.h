0cf28090d849558a7140d36b2314e180b520c05b
JackofAll312 <keenansimpson312@gmail.com>
2020-06-24 09:40:05 -0400

    Hamish

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "SHAMUS"
+#define CUSTOM_MACHINE_NAME "HAMISH"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
